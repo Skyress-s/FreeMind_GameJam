c672ff87e98dae2fcb314726999af2e0c11cef34
Mathias M <62551684+Skyress-s@users.noreply.github.com>
2022-01-28 20:14:36 +0100

    innit

---
+++ Source/FreeMind/FreeMindGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FreeMindGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FREEMIND_API AFreeMindGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
